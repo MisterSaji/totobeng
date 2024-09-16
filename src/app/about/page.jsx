76ef453cae66ea2ebf0db20b482891aa3b2d197f
MisterSaji <164487904+MisterSaji@users.noreply.github.com>
2024-09-16 15:02:55 +0200

    Add files via upload

---
+++ src/app/about/page.jsx
@@ -0,0 +1,53 @@
+export default function about() {
+  return (
+    <div>
+      <main class="grid">
+        <img src="https://picsum.photos/700/400/?blur" alt="Mountains" />
+        <div class="welcome">
+          <h2>Mountains evert tells alot about me</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+            cumque molestiae culpa nisi voluptatibus laudantium mollitia
+            obcaecati omnis in, ea adipisci, deleniti praesentium? Similique
+            aspernatur amet dolor illum, ea tempore animi laborum. Temporibus
+            explicabo, omnis molestiae cum eos quidem officia.
+          </p>
+        </div>
+        <h2>Services</h2>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/contact/page.jsx
@@ -0,0 +1,53 @@
+export default function contact() {
+  return (
+    <div>
+      <main class="grid">
+        <img src="https://picsum.photos/id/237/700/400" alt="Mountains" />
+        <div class="welcome">
+          <h2>Contact my dog</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+            cumque molestiae culpa nisi voluptatibus laudantium mollitia
+            obcaecati omnis in, ea adipisci, deleniti praesentium? Similique
+            aspernatur amet dolor illum, ea tempore animi laborum. Temporibus
+            explicabo, omnis molestiae cum eos quidem officia.
+          </p>
+        </div>
+        <h2>Services</h2>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+        <div class="card">
+          <h3>Hello.</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+            itaque.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
